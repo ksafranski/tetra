6f4efc42fd00721bd858ea8e49fac78ca3065702
fluidbyte <kent@fluidbyte.net>
2014-06-29 14:40:43 -0400

    Structuring blob controller

--- libs/blobs.js
+++ libs/blobs.js
@@ -1,1 +1,45 @@
+module.exports = function (req) {
 
+  var self = this;
+
+  // Retrieve a blob
+  var read = function () {
+
+  };
+
+  // Create new blob
+  var create = function () {
+
+  };
+
+  // Update existing blob
+  var update = function () {
+
+  };
+
+  // Delete blob
+  var del = function () {
+
+  };
+
+  // Check method
+  switch (req.method) {
+  case 'GET':
+    read();
+    break;
+  case 'POST':
+    create();
+    break;
+  case 'PUT':
+    update();
+    break;
+  case 'DELETE':
+    del();
+    break;
+  default:
+    // Unsupported method
+    self.respond(405);
+    return false;
+  }
+
+};
