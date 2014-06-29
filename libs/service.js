85a2b79d97d17c7e9e6b77bdb56461b7cd4d2094
fluidbyte <kent@fluidbyte.net>
2014-06-28 21:36:24 -0400

    Adding CORS config and middleware

--- libs/service.js
+++ libs/service.js
@@ -23,6 +23,19 @@ Service.prototype.start = function () {
     output('error', 'Missing config file: /conf/service.json');
     return false;
   }
+  // CORS
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', config.cors);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // Intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+  };
+  app.use(allowCrossDomain);
   app.use(connectDomain());
   // Body parser
   app.use(bodyParser.urlencoded({
