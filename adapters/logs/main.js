ceb8e27802b1be7751a928325f2795da8d7c0cfb
fluidbyte <kent@fluidbyte.net>
2014-07-03 18:12:33 -0400

    Adding core logging method

---
+++ adapters/logs/main.js
@@ -0,0 +1,74 @@
+var Conn = function () {
+
+};
+
+Conn.prototype.save = function (log) {
+  console.log(log);
+};
+
+Conn.prototype.log = function (req, res, next) {
+
+  var self = this;
+  var send;
+  var end;
+
+  // Set start time
+  if (!req._startTime) {
+    req._startTime = new Date();
+  }
+
+  // Process request send
+  send = res.send;
+  res.send = function (_status, _body) {
+    res.send = send;
+    if (_body) {
+      return res.send(_status, _body);
+    } else {
+      return res.send(_status);
+    }
+  };
+
+  // Process request end
+  end = res.end;
+  res.end = function (c, e) {
+    var ip, len, obj;
+    res.end = end;
+    res.end(c, e);
+    if (req.ip) {
+      ip = req.ip;
+    } else if (req.socket && req.socket.socket) {
+      ip = req.socket.socket.remoteAddress;
+    }
+    len = parseInt(res.getHeader('Content-Length') || '0', 10) / 1024;
+    // Create obj
+    obj = {
+      at: req._startTime,
+      duration: (new Date()) - req._startTime,
+      status: res.statusCode,
+      ip: ip,
+      path: req.path || req.originalUrl || req.url,
+      method: req.method,
+      respKb: Math.round(len * 10) / 10
+    };
+    if (req.headers) { // Headers?
+      obj.headers = req.headers;
+    }
+    // Additional log options...
+    if (req.body) { // Body?
+      obj.body = req.body;
+    }
+    if (req.query) { // Query?
+      obj.qs = req.query;
+    }
+    if (req.params) { // Params?
+      obj.params = req.params;
+    }
+    if (req.files) { // Files?
+      obj.files = Object.keys(req.files);
+    }
+
+    // Save
+    self.save(obj);
+  };
+  return next();
+};
