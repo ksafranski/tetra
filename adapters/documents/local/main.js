40e41288591e84a8e4073e5179460a64cd16d240
fluidbyte <kent@fluidbyte.net>
2014-07-05 15:55:48 -0400

    Setting up core store objects and procedures

--- adapters/documents/local/main.js
+++ adapters/documents/local/main.js
@@ -1,6 +1,25 @@
+var Datastore = require('nedb');
+var config = require('./../../../libs/config');
+var fs = require('fs');
 /* jshint unused: false */
 var Conn = function () {
+  // Set store object
+  this.store = {};
+  // Set base
+  this.base = __dirname + '/../../../' + config.service.documents.path;
+  // Ensure directory exists
+  if (!fs.existsSync(this.base)) {
+    // No? Well build it...
+    fs.mkdirSync(this.base);
+  }
+};
 
+// Sets the current store
+Conn.prototype.setStore = function (coll) {
+  this.store = new Datastore({
+    filename: this.base + coll + '.db',
+    autoload: true
+  });
 };
 
 Conn.prototype.find = function (coll, cursor, query, orderby, cb) {
