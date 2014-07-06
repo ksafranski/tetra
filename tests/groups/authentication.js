81dc8d5763bdcb2967d876a7c278c3fb830e7e6f
fluidbyte <kent@fluidbyte.net>
2014-07-06 17:56:45 -0400

    Building tests and running eachSeries to process

--- tests/groups/authentication.js
+++ tests/groups/authentication.js
@@ -1,9 +1,20 @@
 module.exports = {
-  // Test authentication
-  authentication: {
+  // Test BasicAuth authentication
+  BasicAuth: {
     method: 'GET',
     url: '/',
+    payload: false,
     resultCode: 404,
     result: false
-  } 
+  },
+  Session: {
+    method: 'POST',
+    url: '/session',
+    payload: {
+      'username': 'admin',
+      'password': 'password123'
+    },
+    resultCode: 201,
+    result: false
+  }
 };
--- tests/runner.js
+++ tests/runner.js
@@ -1,15 +1,16 @@
 var spawn = require('child_process').spawn;
 var output = require('./../libs/output');
 var readline = require('readline');
+var async = require('async');
 
 // Test groups
-var tests = {};
+var tests = [];
 var groups = [
   'authentication'
 ];
 
 // Start server
-var proc = spawn('node', [ 'index.js' ], {
+var proc = spawn('node', ['index.js'], {
   cwd: __dirname + '/../',
   stdio: ['ipc']
 });
@@ -23,16 +24,31 @@ readline.createInterface({
 });
 
 // Handle process exit
-proc.on('close', function (code) {
+proc.on('close', function () {
   output('success', 'Service stopped');
 });
 
 // Process has fully started
-proc.on('message', function() {
-  
+proc.on('message', function () {
+  // Build tests
+  output('success', 'Building tests');
+  for (var i = 0, z = groups.length; i < z; i++) {
+    var group = require('./groups/' + groups[i]);
+    for (var test in group) {
+      tests.push({
+        name: groups[i] + ' >> ' + test,
+        specs: group[test]
+      });
+    }
+  }
+  output('success', 'Running tests');
+  async.eachSeries(tests, function (test, callback) {
+    console.log('RUNNING ' + test.name);
+    callback();
+  }, function (err) {
+    if (err) {
+      output('error', 'TEST FAILED');
+    }
+    proc.kill();
+  });
 });
-
-
-setTimeout(function () {
-  proc.kill();
-}, 3000);
