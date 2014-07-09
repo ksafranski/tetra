ae12105ca57494ad4f4aca61619a3ca1b7406dcf
fluidbyte <kent@fluidbyte.net>
2014-07-08 20:47:46 -0400

    Adding missing next() call on error handler

--- libs/service.js
+++ libs/service.js
@@ -83,20 +83,23 @@ Service.prototype.start = function () {
     app.use(logger);
   }
   // General error catch
-  app.use(function (err, req, res) {
+  app.use(function (err, req, res, next) {
     if (err) {
       if (err.toString().indexOf('Unexpected token') !== -1) {
         // The crap bodyParser doesn't seem to catch...
         res.send(400, {
           response: 'Invalid format of request body'
         });
+        return false;
       } else {
         // General f-up's
         res.send(500, {
           response: 'Error processing request'
         });
+        return false;
       }
     }
+    next();
   });
   // Bind endpoints to api module
   app.all('/*', processor).use(errorHandler);
